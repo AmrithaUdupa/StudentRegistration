7fea275a4d49ce4e2e100eca72fdb37815368613
AmrithaUdupa <udupaamritha54@gmail.com>
2024-06-28 11:52:57 +0530

    Updated

--- Backend/demo/src/main/java/com/example/demo/Controller/StudentControllerLogin.java
+++ Backend/demo/src/main/java/com/example/demo/Controller/StudentControllerLogin.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.Service.StudentServices;
